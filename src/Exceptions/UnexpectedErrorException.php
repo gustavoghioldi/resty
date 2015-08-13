44bfb0dec87ce0e783ccbfb1d8348ca920fa285e
Federico Mosto <mosto.federico@gmail.com>
2015-08-13 19:51:29 -0300

    Add - Se agrega nuevo tipo de excepcion

---
+++ src/Exceptions/UnexpectedErrorException.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * InvalidEnvironmentException.php
+ *
+ * PHP version 5.6+
+ *
+ * Copyright (c) 2015 Federico Lozada Mosto <mosto.federico@gmail.com>
+ * For the full copyright and license information, please view the LICENSE file that was distributed
+ * with this source code.
+ *
+ * @category  Resty
+ * @package   Resty\Exceptions
+ * @author    Federico Lozada Mosto <mosto.federico@gmail.com>
+ * @copyright 2015 Federico Lozada Mosto <mosto.federico@gmail.com>
+ * @license   MIT License (http://www.opensource.org/licenses/mit-license.php)
+ * @link      http://www.mostofreddy.com.ar
+ */
+namespace Resty\Exceptions;
+
+use Resty\Exceptions\RestyTraitException;
+// Symfony - HttpKernel
+use Symfony\Component\HttpKernel\Exception\HttpException;
+
+/**
+ * InvalidEnvironmentException
+ *
+ * @category  Resty
+ * @package   Resty\Exceptions
+ * @author    Federico Lozada Mosto <mosto.federico@gmail.com>
+ * @copyright 2015 Federico Lozada Mosto <mosto.federico@gmail.com>
+ * @license   MIT License (http://www.opensource.org/licenses/mit-license.php)
+ * @link      http://www.mostofreddy.com.ar
+ */
+class UnexpectedErrorException extends HttpException
+{
+    use RestyTraitException;
+    const MSG = "Internal server error";
+    const CODE = 100003;
+
+    /**
+     * Constructor.
+     *
+     * @param string     $message  The internal exception message
+     * @param \Exception $previous The previous exception
+     * @param int        $code     The internal exception code
+     */
+    public function __construct($message = null, \Exception $previous = null, $code = 0)
+    {
+        parent::__construct(500, static::MSG, $previous, array(), static::CODE);
+    }
+}
